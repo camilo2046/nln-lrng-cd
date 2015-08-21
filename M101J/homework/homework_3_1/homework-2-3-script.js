ee6c6f3d502df750c3d5159cad94313c3cd5b66a
camilo2046 <camilo2046@gmail.com>
2015-08-20 23:44:05 -0500

    Edit Week 3 - Homework 1
    Edit Week 3 - Homework 1

--- M101J/homework/homework_3_1/homework-2-3-script.js
+++ M101J/homework/homework_3_1/homework-2-3-script.js
@@ -4,14 +4,14 @@ student_id_grades_cursor = db.students.find ();
 
 while (student_id_grades_cursor.hasNext ()) 
 {
-	current_cursor_grade = student_id_grades_cursor.next ();
+	current_cursor_grade = student_id_grades_cursor.next (); null;
 	min_homework_grade   = null;
 	
-	for (var idx_score = 0; i < current_cursor_grade["scores"]; i++)
+	for (var idx_score = 0; idx_score < current_cursor_grade["scores"].length; idx_score++) 
 	{
-		if (current_cursor_grade["scores"][idx_score]["type"] == "homework")
+		if (current_cursor_grade["scores"][idx_score]["type"] == "homework") 
 		{
-			if ((min_homework_grade == null) || (min_homework_grade > current_cursor_grade["scores"][idx_score]["score"]))
+			if ((min_homework_grade == null) || (min_homework_grade > current_cursor_grade["scores"][idx_score]["score"])) 
 			{
 				min_homework_grade = current_cursor_grade["scores"][idx_score]["score"];
 			}
